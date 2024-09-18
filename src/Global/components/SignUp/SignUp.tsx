53dca15a7b5b1aae5fd97134821e51fc82ab3771
AronM <strixparalel@gmail.com>
2024-09-18 15:19:31 +0200

    v13

--- src/Global/components/SignUp/SignUp.tsx
+++ src/Global/components/SignUp/SignUp.tsx
@@ -1,7 +1,10 @@
 import { v4 } from "uuid";
 import "./SignUp.scss";
+import moment from "moment";
 
 function SignUp() {
+  //A way to generate year values from 1905 to 2024 without writing them out one by one
+
   const endYear: number = new Date().getFullYear();
   const years: { year: number; id: string }[] = [];
 
@@ -12,7 +15,99 @@ function SignUp() {
   };
 
   birthdayYears();
-  console.log(years);
+
+  //List of months, might need improvement
+
+  const currentMonth = new Date().getMonth() + 1;
+  console.log(currentMonth);
+
+  const months: { month: string; number: number; id: string }[] = [
+    {
+      month: "January",
+      number: 1,
+      id: v4(),
+    },
+
+    {
+      month: "February",
+      number: 2,
+      id: v4(),
+    },
+
+    {
+      month: "March",
+      number: 3,
+      id: v4(),
+    },
+
+    {
+      month: "April",
+      number: 4,
+      id: v4(),
+    },
+
+    {
+      month: "May",
+      number: 5,
+      id: v4(),
+    },
+
+    {
+      month: "June",
+      number: 6,
+      id: v4(),
+    },
+
+    {
+      month: "July",
+      number: 7,
+      id: v4(),
+    },
+
+    {
+      month: "August",
+      number: 8,
+      id: v4(),
+    },
+
+    {
+      month: "September",
+      number: 9,
+      id: v4(),
+    },
+
+    {
+      month: "October",
+      number: 10,
+      id: v4(),
+    },
+
+    {
+      month: "November",
+      number: 11,
+      id: v4(),
+    },
+
+    {
+      month: "December",
+      number: 12,
+      id: v4(),
+    },
+  ];
+
+  //List of days
+
+  const currentDay = moment().format("D");
+
+  const days: { day: number; id: string }[] = [];
+
+  const generateDays = () => {
+    for (let i = 1; i <= 31; i++) {
+      days.push({ day: i, id: v4() });
+    }
+  };
+
+  generateDays();
 
   return (
     <>
@@ -51,15 +146,27 @@ function SignUp() {
                   className="birthday-selector selector"
                   name="month"
                   id="month"
+                  defaultValue={currentMonth}
                 >
-                  <option value="Sep">Sep</option>
+                  {months.map((month) => (
+                    <option key={month.id} value={month.number}>
+                      {month.month}
+                    </option>
+                  ))}
                 </select>
                 <select
                   className="birthday-selector selector"
                   name="day"
                   id="day"
+                  defaultValue={currentDay}
                 >
-                  <option value="17">17</option>
+                  {days.reverse().map((day) => {
+                    return (
+                      <option key={day.id} value={day.day}>
+                        {day.day}
+                      </option>
+                    );
+                  })}
                 </select>
                 <select
                   className="birthday-selector selector"
@@ -110,11 +217,9 @@ function SignUp() {
                     <option value="she">
                       She: "Wish her a happy birthday!"
                     </option>
-                    <option value="he">
-                      She: "Wish him a happy birthday!"
-                    </option>
+                    <option value="he">He: "Wish him a happy birthday!"</option>
                     <option value="them">
-                      She: "Wish them a happy birthday!"
+                      They: "Wish them a happy birthday!"
                     </option>
                   </select>
                   <label htmlFor="pronoun-selector">
