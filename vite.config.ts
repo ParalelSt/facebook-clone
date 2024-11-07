02d7773cf44e2cb3bc42232157e583e56f96cdda
AronM <strixparalel@gmail.com>
2024-11-07 11:50:47 +0100

    v53 - netlify commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
+declare const __dirname: string;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
