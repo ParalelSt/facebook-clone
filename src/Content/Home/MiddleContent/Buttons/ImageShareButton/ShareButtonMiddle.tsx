7bf5e1ac154b85a0698356a7a698aad8f4ae1904
AronM <strixparalel@gmail.com>
2024-11-21 17:24:57 +0100

    v68 - added the search input bar in shareDropDownMiddle

--- src/Content/Home/MiddleContent/Buttons/ImageShareButton/ShareButtonMiddle.tsx
+++ src/Content/Home/MiddleContent/Buttons/ImageShareButton/ShareButtonMiddle.tsx
@@ -1,7 +1,17 @@
 import "Content/Home/MiddleContent/Buttons/ImageShareButton/ShareDropDownStyles.scss";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
 const ShareButtonMiddle = () => {
-  return <div className="share-button-drop-down-middle"></div>;
+  return (
+    <div className="share-button-drop-down-middle">
+      <div className="share-button-drop-down-middle-input">
+        <div className="share-button-drop-down-middle-input-icon">
+          <FaMagnifyingGlass />
+        </div>
+        <input type="text" placeholder="Search for people and groups" />
+      </div>
+    </div>
+  );
 };
 
 export default ShareButtonMiddle;
