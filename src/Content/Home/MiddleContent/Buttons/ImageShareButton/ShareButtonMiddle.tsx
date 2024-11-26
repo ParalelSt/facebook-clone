7f1ad5ec8f69da2c1e5768eb75690add94bf3b8d
AronM <strixparalel@gmail.com>
2024-11-26 09:01:25 +0100

    v71 - added contact display for share drop down

--- src/Content/Home/MiddleContent/Buttons/ImageShareButton/ShareButtonMiddle.tsx
+++ src/Content/Home/MiddleContent/Buttons/ImageShareButton/ShareButtonMiddle.tsx
@@ -1,14 +1,29 @@
 import { ContactListType } from "Content/Home/Home";
 import "Content/Home/MiddleContent/Buttons/ImageShareButton/ShareDropDownStyles.scss";
-import { FaMagnifyingGlass } from "react-icons/fa6";
+import { useRef, useState } from "react";
+import { FaLock, FaMagnifyingGlass } from "react-icons/fa6";
 
 interface ShareButtonMiddleProps {
   contacts: ContactListType[];
 }
 
 const ShareButtonMiddle = ({ contacts }: ShareButtonMiddleProps) => {
+  const [isGroup, setIsGroup] = useState(false);
+  const checkBoxRef = useRef<HTMLInputElement | null>(null);
+
   return (
     <div className="share-button-drop-down-middle">
+      {isGroup && (
+        <div className="share-button-drop-down-middle-text">
+          <div className="lock-icon">
+            <FaLock />
+          </div>
+          <span>
+            Only members can see who's in the group and what they post.
+            Non-members will get an invite to join.
+          </span>
+        </div>
+      )}
       <div className="share-button-drop-down-middle-input">
         <div className="share-button-drop-down-middle-input-icon">
           <FaMagnifyingGlass />
@@ -17,7 +32,15 @@ const ShareButtonMiddle = ({ contacts }: ShareButtonMiddleProps) => {
       </div>
       <div className="share-button-drop-down-middle-content-container">
         {contacts.map((contact) => {
-          return <div className="contact">{contact.username}</div>;
+          return (
+            <div className="contact" key={contact.id}>
+              <div className="profile-picture">
+                <img src={contact.image} alt="contact profile picture" />
+              </div>
+              <h4 className="username">{contact.username}</h4>
+              <input type="checkbox" ref={checkBoxRef} />
+            </div>
+          );
         })}
       </div>
     </div>
