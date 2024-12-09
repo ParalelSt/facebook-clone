e6ef7f456b5ac56b4b53d03300336cea29055082
AronM <strixparalel@gmail.com>
2024-12-09 12:09:56 +0100

    v79 - added Share Drop Down Bottom input

--- src/Content/Home/MiddleContent/Buttons/ImageShareButton/ShareButtonBottom.tsx
+++ src/Content/Home/MiddleContent/Buttons/ImageShareButton/ShareButtonBottom.tsx
@@ -1,7 +1,20 @@
 import "Content/Home/MiddleContent/Buttons/ImageShareButton/ShareDropDownStyles.scss";
+import { FaFacebookMessenger } from "react-icons/fa6";
 
 const ShareButtonBottom = () => {
-  return <div className="share-button-drop-down-bottom"></div>;
+  return (
+    <div className="share-button-drop-down-bottom">
+      <div className="share-button-drop-down-bottom-input">
+        <input type="text" placeholder="Add an optional message here..." />
+      </div>
+      <div className="share-button-drop-down-bottom-button-container">
+        <div className="share-button-drop-down-bottom-icon-container">
+          <FaFacebookMessenger />
+        </div>
+        <h4 className="share-button-drop-down-bottom-button-text">Send</h4>
+      </div>
+    </div>
+  );
 };
 
 export default ShareButtonBottom;
