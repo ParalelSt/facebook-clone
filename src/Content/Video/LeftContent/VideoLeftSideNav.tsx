d793305081dbf468214c73877264eecf95adf963
AronM <strixparalel@gmail.com>
2024-12-24 08:58:49 +0100

    v88 - added side nav buttons for video route

--- src/Content/Video/LeftContent/VideoLeftSideNav.tsx
+++ src/Content/Video/LeftContent/VideoLeftSideNav.tsx
@@ -1,5 +1,9 @@
 import "Content/Video/LeftContent/VideoLeftSideNav.scss";
 import ContentContainer from "Global/components/ContentContainer/ContentContainer";
+import { FaBookmark, FaRocket } from "react-icons/fa6";
+import { MdOutlineMovieCreation } from "react-icons/md";
+import { PiMonitorPlay } from "react-icons/pi";
+import { RiClapperboardFill, RiLiveFill } from "react-icons/ri";
 
 const VideoLeftSideNav = () => {
   return (
@@ -15,7 +19,32 @@ const VideoLeftSideNav = () => {
           </div>
         </div>
       </div>
-      <div className="video-left-side-nav-middle"></div>
+      <div className="video-left-side-nav-middle">
+        <div className="vide-left-side-nav-middle-button">
+          <PiMonitorPlay />
+          <span>Home</span>
+        </div>
+        <div className="vide-left-side-nav-middle-button">
+          <RiLiveFill />
+          <span>Live</span>
+        </div>
+        <div className="vide-left-side-nav-middle-button">
+          <RiClapperboardFill />
+          <span>Reels</span>
+        </div>
+        <div className="vide-left-side-nav-middle-button">
+          <MdOutlineMovieCreation />
+          <span>Shows</span>
+        </div>
+        <div className="vide-left-side-nav-middle-button">
+          <FaRocket />
+          <span>Explore</span>
+        </div>
+        <div className="vide-left-side-nav-middle-button">
+          <FaBookmark />
+          <span>Saved Videos</span>
+        </div>
+      </div>
       <div className="video-left-side-nav-bottom"></div>
     </ContentContainer>
   );
